27c67e182da26a6cc9998a6052fe32ebf703d1fd
Avik Sam <68726802+sam-as@users.noreply.github.com>
2022-09-27 12:26:14 +0530

    Add files via upload
    Requirements
    Python
    Libraries required
    Pandas: pandas - Python Data Analysis Library (pydata.org)
    Geopandas: GeoPandas 0.11.0 — GeoPandas 0.11.0+0.g1977b50.dirty documentation
    Matplotlib - Matplotlib — Visualization with Python
    Seaborn - seaborn: statistical data visualization — seaborn 0.12.0 documentation (pydata.org)
    
    Python – install from Download Python | Python.org
    Click on add to Path
    Use Pip to install Pandas, Matplotlib and Seaborn
    For ex: pip install pandas
    For Geopandas, libraries – GDAL & Fiona is required
    Pip direct method doesn’t work
    We will download wheel file and use pip. 
    Website: Archived: Python Extension Packages for Windows - Christoph Gohlke (uci.edu) 
    Copy the location of the downloaded wheel package and use pip
    pip install C:\\Users\\Avik\\Downloads\\netCDF4-1.6.0-cp310-cp310-win_amd64.whl

---
+++ Git_COVID19_mapping/stats_class_maps_code.py
@@ -0,0 +1,84 @@
+#%% 
+import pandas as pd
+import geopandas as gd
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.cm as cm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+#%%
+path = r"-------Shape file data\State_shp\Admin2.shp"
+gdf = gd.read_file(path)
+
+gdf = gdf.to_crs("EPSG:4326")
+
+# %%
+path2=  r"----------------diseasedata_population.xlsx"
+df= pd.read_excel(path2)
+# %%
+gdf = gdf.rename(columns={'ST_NM': 'State/UTs'})
+
+# %%
+da = gdf.merge(df, on = "State/UTs")
+#%% getting the longitudes Aand the latitudes
+da['Longitude'] = da['geometry'].centroid.x
+da['Latitude'] = da['geometry'].centroid.y
+# %%
+sns.set(style='ticks')
+plt.rcParams["figure.figsize"] = (8,9)
+
+fig, ax = plt.subplots()
+divider = make_axes_locatable(ax)
+
+ax = da.plot("State/UTs", edgecolor='black', facecolor = "none", ax=ax )
+
+#da.apply(lambda x: ax.annotate(text=x['State/UTs'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
+
+#plt.show()
+
+# adding the mortalities
+
+ax1 = ax.scatter(
+    x = da['Longitude'],
+    y = da['Latitude'],
+    s= da['Deaths']/100,
+    alpha=0.7, 
+    color='red',
+)
+
+handles, labels = ax1.legend_elements(prop="sizes", alpha=0.5)
+legend2 = ax.legend(handles, labels, loc='center left', bbox_to_anchor=(-0.30, 0.5), 
+     title="Mortality*100", fancybox=False)
+
+plt.tight_layout()
+plt.show()
+
+#%% making a chloropleth + adding the scatter matrix
+fig, ax = plt.subplots()
+divider = make_axes_locatable(ax)
+
+cax = divider.append_axes("right", size="5%", pad=0.01)
+
+ax = da.plot("Total Cases", edgecolor='black', cmap = 'CMRmap_r', 
+    alpha= 0.6, 
+    legend=True,
+    legend_kwds = {'label': "Positive COVID-19 Cases", "alpha" : 0.6}, ax = ax, cax = cax )
+#%% 
+ax1 = ax.scatter(
+    x = da['Longitude'],
+    y = da['Latitude'],
+    s= da['Deaths']/100,
+    alpha=0.7, 
+    color='red',
+)
+
+handles, labels = ax1.legend_elements(prop="sizes", alpha=0.5)
+legend2 = ax.legend(handles, labels, loc='center left', bbox_to_anchor=(-0.35, 0.5), 
+     title="Mortality*100", fancybox=False)
+
+plt.tight_layout()
+
+plt.show()
